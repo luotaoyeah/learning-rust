30bdbefb2e2b65e5512d1e122c9ee8b2912b1aed
luotao <luotaoyeah@gmail.com>
2018-10-24 20:42:49 +0800

    the-rust-programming-language: 3.5: return from loop

---
+++ src/books/the_rust_programming_language_2018e/ch_03/ch_03_05/ch_03_05_04/ch_03_05_04_02.rs
@@ -0,0 +1,26 @@
+/*
+   Control Flow
+       Repetition with Loops
+           Returning from loops
+ */
+
+pub fn fn_03_05_04_02() {
+    println!("-------------------------------------------------- 01");
+    {
+        /*
+           如果 break 后面跟上一个 expression，则该 expression 会返回成为 loop 的值；
+         */
+
+        let mut n: u32 = 1;
+
+        let m: u32 = loop {
+            if n >= 100 {
+                break n / 2;
+            }
+
+            n += 1;
+        };
+
+        println!("m: {}", m); // m: 50
+    }
+}
