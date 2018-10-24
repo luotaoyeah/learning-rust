ee1357aa166604643af39b91e3069a25546b47ef
luotao <luotaoyeah@gmail.com>
2018-10-24 20:18:08 +0800

    the-rust-programming-language: 3.5: using if in a let statement: same type of each arm

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod books;
 
 fn main() {
-    books::the_rust_programming_language_2018e::ch_03::ch_03_05::ch_03_05_03::ch_03_05_03_01::fn_03_05_03_01();
+    books::the_rust_programming_language_2018e::ch_03::ch_03_05::ch_03_05_03::ch_03_05_03_02::fn_03_05_03_02();
 }
