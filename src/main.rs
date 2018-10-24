0c1970066abb69feb4c987394dfcd83564ffe986
luotao <luotaoyeah@gmail.com>
2018-10-24 21:02:08 +0800

    the-rust-programming-language: 3.5: while

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod books;
 
 fn main() {
-    books::the_rust_programming_language_2018e::ch_03::ch_03_05::ch_03_05_04::ch_03_05_04_02::fn_03_05_04_02();
+    books::the_rust_programming_language_2018e::ch_03::ch_03_05::ch_03_05_04::ch_03_05_04_03::fn_03_05_04_03();
 }
