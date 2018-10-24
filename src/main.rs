350aa30bc1750f7410021656933a4f679fd5ec72
luotao <luotaoyeah@gmail.com>
2018-10-24 22:19:14 +0800

    the-rust-programming-language: 3.5: fibonacci

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod books;
 
 fn main() {
-    books::the_rust_programming_language_2018e::ch_03::ch_03_05::ch_03_05_04::ch_03_05_04_05::fn_03_05_04_05();
+    books::the_rust_programming_language_2018e::ch_03::ch_03_05::ch_03_05_04::ch_03_05_04_06::fn_03_05_04_06();
 }
