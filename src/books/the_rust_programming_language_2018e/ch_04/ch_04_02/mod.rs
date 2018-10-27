3b93f2202917f4febfc772151f34abe87c9c4ca1
luotao <luotaoyeah@gmail.com>
2018-10-27 23:00:21 +0800

    the-rust-programming-language: 4.2: dangling reference

--- src/books/the_rust_programming_language_2018e/ch_04/ch_04_02/mod.rs
+++ src/books/the_rust_programming_language_2018e/ch_04/ch_04_02/mod.rs
@@ -1,1 +1,1 @@
-pub mod ch_04_02_02;
+pub mod ch_04_02_03;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod books;
 
 fn main() {
-    books::the_rust_programming_language_2018e::ch_04::ch_04_02::ch_04_02_02::ch_04_02_02_03::fn_04_02_02_03();
+    books::the_rust_programming_language_2018e::ch_04::ch_04_02::ch_04_02_03::ch_04_02_03_01::fn_04_02_03_01();
 }
