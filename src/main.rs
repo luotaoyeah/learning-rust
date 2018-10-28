318d9117805d8ca3b5191f5d1380b2cb959beb7f
luotao <luotaoyeah@gmail.com>
2018-10-28 17:07:47 +0800

    the-rust-programming-language: 5.1: field init shorthand

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod books;
 
 fn main() {
-    books::the_rust_programming_language_2018e::ch_05::ch_05_01::ch_05_01_04::fn_05_01_04();
+    books::the_rust_programming_language_2018e::ch_05::ch_05_01::ch_05_01_02::ch_05_01_02_01::fn_05_01_02_01();
 }
