934c837f156004c01b7f98b93dba0a9919e10758
luotao <luotaoyeah@gmail.com>
2018-10-28 14:06:09 +0800

    the-rust-programming-language: 4.2: slice: find first world

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod books;
 
 fn main() {
-    books::the_rust_programming_language_2018e::ch_04::ch_04_02::ch_04_02_03::ch_04_02_03_01::fn_04_02_03_01();
+    books::the_rust_programming_language_2018e::ch_04::ch_04_03::ch_04_03_01::ch_04_03_01_01::fn_04_03_01_01();
 }
