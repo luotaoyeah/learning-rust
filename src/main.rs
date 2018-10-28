6ac4fbb5b1998e2da1e6b4f2b426920b01a4825f
luotao <luotaoyeah@gmail.com>
2018-10-28 14:42:54 +0800

    the-rust-programming-language: 4.2: drop start index and end index

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod books;
 
 fn main() {
-    books::the_rust_programming_language_2018e::ch_04::ch_04_03::ch_04_03_02::ch_04_03_02_01::fn_04_03_02_01();
+    books::the_rust_programming_language_2018e::ch_04::ch_04_03::ch_04_03_02::ch_04_03_02_02::fn_04_03_02_02();
 }
