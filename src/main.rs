8b19243688e87ec31259bcea3f5e1ce3134fccb1
luotao <luotaoyeah@gmail.com>
2018-10-28 20:30:05 +0800

    the-rust-programming-language: 5.1: unit-like struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod books;
 
 fn main() {
-    books::the_rust_programming_language_2018e::ch_05::ch_05_01::ch_05_01_04::ch_05_01_04_01::fn_05_01_04_01();
+    books::the_rust_programming_language_2018e::ch_05::ch_05_01::ch_05_01_05::ch_05_01_05_01::fn_05_01_05_01();
 }
