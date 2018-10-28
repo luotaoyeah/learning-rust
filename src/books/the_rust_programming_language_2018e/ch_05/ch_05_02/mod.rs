86dc50f06aa95f02dd7f30be99d17be5c1954453
luotao <luotaoyeah@gmail.com>
2018-10-28 20:47:09 +0800

    the-rust-programming-language: 5.2: an example using struct: using tuple

--- src/books/the_rust_programming_language_2018e/ch_05/ch_05_02/mod.rs
+++ src/books/the_rust_programming_language_2018e/ch_05/ch_05_02/mod.rs
@@ -1,1 +1,1 @@
-pub mod ch_05_02_01;
+pub mod ch_05_02_02;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod books;
 
 fn main() {
-    books::the_rust_programming_language_2018e::ch_05::ch_05_02::ch_05_02_01::ch_05_02_01_01::fn_05_02_01_01();
+    books::the_rust_programming_language_2018e::ch_05::ch_05_02::ch_05_02_02::ch_05_02_02_01::fn_05_02_02_01();
 }
