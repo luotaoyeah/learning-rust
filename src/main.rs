9b393815c788c1e29a3829678f4f7796fa0f5714
luotao <luotaoyeah@gmail.com>
2018-10-28 16:13:43 +0800

    the-rust-programming-language: 5.1: structs are expressions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod books;
 
 fn main() {
-    books::the_rust_programming_language_2018e::ch_05::ch_05_01::ch_05_01_03::fn_05_01_03();
+    books::the_rust_programming_language_2018e::ch_05::ch_05_01::ch_05_01_04::fn_05_01_04();
 }
