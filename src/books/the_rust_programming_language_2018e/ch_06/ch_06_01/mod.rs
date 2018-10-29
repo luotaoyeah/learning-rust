0e442421e17c7e772df2c394d7e20d54170c5728
luotao <luotaoyeah@gmail.com>
2018-10-29 20:02:01 +0800

    the-rust-programming-language: 6.1: enum variants

--- src/books/the_rust_programming_language_2018e/ch_06/ch_06_01/mod.rs
+++ src/books/the_rust_programming_language_2018e/ch_06/ch_06_01/mod.rs
@@ -1,1 +1,1 @@
-pub mod ch_06_01_01;
+pub mod ch_06_01_02;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod books;
 
 fn main() {
-    books::the_rust_programming_language_2018e::ch_06::ch_06_01::ch_06_01_01::ch_06_01_01_01::fn_06_01_01();
+    books::the_rust_programming_language_2018e::ch_06::ch_06_01::ch_06_01_02::ch_06_01_02_01::fn_06_01_02_01();
 }
