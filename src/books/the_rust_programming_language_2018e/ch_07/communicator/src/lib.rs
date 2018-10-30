dfe262e8dd282bf2bedf7152b6919ec7eb957d80
luotao <luotaoyeah@gmail.com>
2018-10-30 21:56:47 +0800

    the-rust-programming-language: 7.1: mod.rs

--- src/books/the_rust_programming_language_2018e/ch_07/communicator/src/lib.rs
+++ src/books/the_rust_programming_language_2018e/ch_07/communicator/src/lib.rs
@@ -1,23 +1,17 @@
 /*
-   通过 `mod` 关键字来定义一个 module
+   将 client 模块的内容移到 client.rs 文件中，
+   此处只保留模块声明；
+   此处的模块声明可以告诉 lib.rs 去引入 client.rs 的内容；
  */
-mod network {
-    /*
-       module 中的内容需要通过 namespace 的方式来访问，如：network::connect()
-     */
-    fn connect() {}
 
-    /*
-       module 中可以定义 inner module
-     */
-    mod client {
-        fn connect() {}
-    }
-}
-
-mod client {
-    fn connect() {}
-}
+/*
+   当使用 mod 引入一个 module 时，有两种方式去定位这个 module，
+   假如 module name 为 foo：
+       1. 当前目录下有一个 foo.rs 文件；
+       2. 当前目录下有一个 foo 目录，下面有一个 mod.rs 文件；
+ */
+mod client;
+mod network;
 
 #[cfg(test)]
 mod tests {
