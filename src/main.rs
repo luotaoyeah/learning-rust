f9ff65a3b53620b2f977b3f7ed074ca32a167419
luotao <luotaoyeah@gmail.com>
2018-10-30 20:08:07 +0800

    the-rust-programming-language: 6.2: match: matching with Option<T>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod books;
 
 fn main() {
-    books::the_rust_programming_language_2018e::ch_06::ch_06_02::ch_06_02_02::ch_06_02_02_01::fn_06_02_02_01();
+    books::the_rust_programming_language_2018e::ch_06::ch_06_02::ch_06_02_03::ch_06_02_03_01::fn_06_02_03_01();
 }
