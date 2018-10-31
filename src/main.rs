10610554ee8d69763168f175a78ff8ac9a42cca7
luotao <luotaoyeah@gmail.com>
2018-10-31 20:22:04 +0800

    the-rust-programming-language: 7.3: enum as namespace

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod books;
 
 fn main() {
-    books::the_rust_programming_language_2018e::ch_07::ch_07_03::ch_07_03_02::ch_07_03_02_01::fn_07_03_02_01();
+    books::the_rust_programming_language_2018e::ch_07::ch_07_03::ch_07_03_02::ch_07_03_02_02::fn_07_03_02_02();
 }
