205f50ca831b307142b187e4c1d5d611c4bbe79d
luotao <luotaoyeah@gmail.com>
2018-11-01 19:56:13 +0800

    the-rust-programming-language: 8.2: create a string

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod books;
 
 fn main() {
-    books::the_rust_programming_language_2018e::ch_08::ch_08_02::ch_08_02_01::ch_08_02_01_01::fn_08_01_06_01();
+    books::the_rust_programming_language_2018e::ch_08::ch_08_02::ch_08_02_02::ch_08_02_02_01::fn_08_02_02_01();
 }
