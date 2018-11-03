e75f8c0fb6a0c1bea8cad1edc41c6ff7d67c6e9f
luotao <luotaoyeah@gmail.com>
2018-11-03 17:38:42 +0800

    the-rust-programming-language: 10.2: restriction

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod books;
 use books::the_rust_programming_language_2018e::ch_10;
 
 fn main() {
-    ch_10::ch_10_02::ch_10_02_01::ch_10_02_01_01::fn_10_02_01_01();
+    ch_10::ch_10_02::ch_10_02_01::ch_10_02_01_02::fn_10_02_01_02();
 }
