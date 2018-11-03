74f53b35bfedaee3b3e3a1388a4eeb52943d0f11
luotao <luotaoyeah@gmail.com>
2018-11-03 20:15:32 +0800

    the-rust-programming-language: 10.2: trait: returning trait

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod books;
 use books::the_rust_programming_language_2018e::ch_10;
 
 fn main() {
-    ch_10::ch_10_02::ch_10_02_03::ch_10_02_03_04::fn_10_02_03_04();
+    ch_10::ch_10_02::ch_10_02_04::ch_10_02_04_01::fn_10_02_04_01();
 }
