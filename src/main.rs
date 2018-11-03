3b1c474a68676ee72091bdeb22bd5c6456be89ca
luotao <luotaoyeah@gmail.com>
2018-11-03 08:17:33 +0800

    the-rust-programming-language: 9.2: propagate errors

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod books;
 use books::the_rust_programming_language_2018e::ch_09;
 
 fn main() {
-    ch_09::ch_09_02::ch_09_02_03::ch_09_02_03_01::fn_09_02_03_01();
+    ch_09::ch_09_02::ch_09_02_04::ch_09_02_04_01::fn_09_02_04_01();
 }
