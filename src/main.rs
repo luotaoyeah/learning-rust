325e2bcca0fb1b0b557a9c22ad3c6ab15b1282cd
luotao <luotaoyeah@gmail.com>
2018-11-03 14:03:40 +0800

    the-rust-programming-language: 10.1: generic struct

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod books;
 use books::the_rust_programming_language_2018e::ch_10;
 
 fn main() {
-    ch_10::ch_10_01::ch_10_01_01::ch_10_01_01_02::fn_10_01_01_02();
+    ch_10::ch_10_01::ch_10_01_02::ch_10_01_02_01::fn_10_01_02_01();
 }
