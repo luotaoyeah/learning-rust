284deeaa25c9fea074969fc029187e71f53b13d6
luotao <luotaoyeah@gmail.com>
2018-11-03 14:44:29 +0800

    the-rust-programming-language: 10.1: generic method in generic impl

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod books;
 use books::the_rust_programming_language_2018e::ch_10;
 
 fn main() {
-    ch_10::ch_10_01::ch_10_01_04::ch_10_01_04_02::fn_10_01_04_02();
+    ch_10::ch_10_01::ch_10_01_04::ch_10_01_04_03::fn_10_01_04_03();
 }
