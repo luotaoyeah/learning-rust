85562b53dd233fc383dc44cfa2c33817cd53f7cb
luotao <luotaoyeah@gmail.com>
2018-11-03 19:54:21 +0800

    the-rust-programming-language: 10.2: trait: multiple trait bounds with +

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod books;
 use books::the_rust_programming_language_2018e::ch_10;
 
 fn main() {
-    ch_10::ch_10_02::ch_10_02_03::ch_10_02_03_02::fn_10_02_03_02();
+    ch_10::ch_10_02::ch_10_02_03::ch_10_02_03_03::fn_10_02_03_03();
 }
