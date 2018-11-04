13d8e44623059b455e2a8af652c57a1f178bf69e
luotao <luotaoyeah@gmail.com>
2018-11-04 17:52:24 +0800

    the-rust-programming-language: 11.1: should_panic

--- src/books/the_rust_programming_language_2018e/ch_11/ch_11_01/mod.rs
+++ src/books/the_rust_programming_language_2018e/ch_11/ch_11_01/mod.rs
@@ -1,1 +1,1 @@
-pub mod ch_11_01_04;
+pub mod ch_11_01_05;
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use books::the_rust_programming_language_2018e::ch_11;
 mod books;
 
 fn main() {
-    ch_11::ch_11_01::ch_11_01_04::ch_11_01_04_01::fn_11_01_04_01();
+    ch_11::ch_11_01::ch_11_01_05::ch_11_01_05_01::fn_11_01_05_01();
 }
