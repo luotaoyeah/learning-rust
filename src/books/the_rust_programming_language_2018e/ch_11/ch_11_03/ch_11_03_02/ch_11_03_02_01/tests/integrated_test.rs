a293baa13cc679393fb9193dbbbf9b864d4379dc
luotao <luotaoyeah@gmail.com>
2018-11-05 20:05:47 +0800

    the-rust-programming-language: 11.3: submodules in integration tests

--- src/books/the_rust_programming_language_2018e/ch_11/ch_11_03/ch_11_03_02/ch_11_03_02_01/tests/integrated_test.rs
+++ src/books/the_rust_programming_language_2018e/ch_11/ch_11_03/ch_11_03_02/ch_11_03_02_01/tests/integrated_test.rs
@@ -1,6 +1,9 @@
 extern crate ch_11_03_02_01;
 
+mod common;
+
 #[test]
 fn inte_test_01() {
+    common::setup();
     assert_eq!(ch_11_03_02_01::add(2, 2), 4);
 }
---
+++ src/books/the_rust_programming_language_2018e/ch_11/ch_11_03/ch_11_03_02/ch_11_03_02_02.rs
@@ -0,0 +1,16 @@
+/*
+   Writing Automated Tests
+       Test Organization
+           Submodules in Integration Tests
+ */
+
+pub fn fn_11_03_02_02() {
+    println!("-------------------------------------------------- 01");
+    {
+        /*
+           tests 下面的每一个文件都会被当成一个单独的 crate，
+           如果需要放置其他工具代码，即不属于集成测试的代码，需要放到 tests 下面的子目录中，
+           如：tests/common/mod.rs
+         */
+    }
+}
