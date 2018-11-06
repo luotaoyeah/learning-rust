023527e3d892dd1fa2c905c4024301c8519aab21
luotao <luotaoyeah@gmail.com>
2018-11-06 20:24:58 +0800

    the-rust-programming-language: 12.3: calling config::new and handling errors

--- src/books/the_rust_programming_language_2018e/ch_12/ch_12_03/mod.rs
+++ src/books/the_rust_programming_language_2018e/ch_12/ch_12_03/mod.rs
@@ -1,1 +1,1 @@
-pub mod ch_12_03_01;
+pub mod ch_12_03_02;
--- src/books/the_rust_programming_language_2018e/ch_12/minigrep/src/main.rs
+++ src/books/the_rust_programming_language_2018e/ch_12/minigrep/src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     // 获取命令行参数列表
     let args: Vec<String> = env::args().collect();
 
-    let config: Config = Config::new(&args);
+    let config: Config = Config::new(&args).unwrap_or_else(|e| {
+        println!("参数解析失败：{}", e);
+        process::exit(1);
+    });
 
     // 读取文件内容
     let text = fs::read_to_string(config.filename).expect("文件读取失败");
@@ -21,10 +25,13 @@ struct Config {
 impl Config {
     /// 解析参数
     /// * `args` 参数列表
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("NOT ENOUGH ARGUMENTS");
+        }
         let query: String = args[1].clone();
         let filename: String = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
