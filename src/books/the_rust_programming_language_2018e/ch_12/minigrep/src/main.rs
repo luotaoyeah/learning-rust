7e803a8e316ac002c823614ab97b9a14a1adbd8e
luotao <luotaoyeah@gmail.com>
2018-11-06 19:43:12 +0800

    the-rust-programming-language: 12.1: saving the argument values in variables

--- src/books/the_rust_programming_language_2018e/ch_12/minigrep/src/main.rs
+++ src/books/the_rust_programming_language_2018e/ch_12/minigrep/src/main.rs
@@ -1,6 +1,11 @@
 use std::env;
 
 fn main() {
+    // 获取命令行参数列表
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    // 将需要的参数保存到变量
+    let query: &String = &args[1];
+    let filename: &String = &args[2];
+    println!("{} {}", query, filename);
 }
