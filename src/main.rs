bff4e0dc0384d63ee2d7d9c8f15a5fb8ba605f1c
luotao <luotaoyeah@gmail.com>
2018-11-08 22:08:14 +0800

    the-rust-programming-language: 13.2: using closures that capture their environment

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use books::the_rust_programming_language_2018e::ch_13;
 mod books;
 
 fn main() {
-    ch_13::ch_13_02::ch_13_02_04::ch_13_02_04_01::fn_13_02_04_01();
+    ch_13::ch_13_02::ch_13_02_05::ch_13_02_05_01::fn_13_02_05_01();
 }
