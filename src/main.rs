6c01ff0bb98cd55e84d164a3fe591e6aab4e477a
luotao <luotaoyeah@gmail.com>
2018-11-08 20:27:32 +0800

    the-rust-programming-language: 13.1: limitations of the cacher implementation

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use books::the_rust_programming_language_2018e::ch_13;
 mod books;
 
 fn main() {
-    ch_13::ch_13_01::ch_13_01_03::ch_13_01_03_02::fn_13_01_03_02();
+    ch_13::ch_13_01::ch_13_01_03::ch_13_01_03_03::fn_13_01_03_03();
 }
