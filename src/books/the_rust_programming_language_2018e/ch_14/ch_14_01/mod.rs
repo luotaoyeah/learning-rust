c0a4b6096f4baecd6040f24c5eb464754e469758
luotao <luotaoyeah@gmail.com>
2018-11-09 20:24:59 +0800

    the-rust-programming-language: 14.2: making useful documentation comments

--- src/books/the_rust_programming_language_2018e/ch_14/ch_14_01/mod.rs
+++ src/books/the_rust_programming_language_2018e/ch_14/ch_14_01/mod.rs
@@ -1,1 +1,1 @@
-pub mod ch_14_01_01;
+pub mod ch_14_01_02;
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use books::the_rust_programming_language_2018e::ch_14;
 mod books;
 
 fn main() {
-    ch_14::ch_14_01::ch_14_01_01::ch_14_01_01_01::fn_14_01_01_01();
+    ch_14::ch_14_01::ch_14_01_02::ch_14_01_02_01::fn_14_01_02_01();
 }
