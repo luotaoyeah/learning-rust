19b513c2bdf9c27a12224d4ae02a177e2a19caf5
luotao <luotaoyeah@gmail.com>
2018-11-10 12:10:55 +0800

    the-rust-programming-language: 15.6: preventing reference cycles: turning an rc<t> into a weak<t>

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use books::the_rust_programming_language_2018e::ch_15;
 mod books;
 
 fn main() {
-    ch_15::ch_15_06::ch_15_06_01::ch_15_06_01_01::fn_15_06_01_01();
+    ch_15::ch_15_06::ch_15_06_02::ch_15_06_02_01::fn_15_06_02_01();
 }
