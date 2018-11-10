95b4f492d3577b2a307e161d1cb089124657f292
luotao <luotaoyeah@gmail.com>
2018-11-10 10:55:14 +0800

    the-rust-programming-language: 15.5: keeping track of borrows at runtime with refcell<t>

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use books::the_rust_programming_language_2018e::ch_15;
 mod books;
 
 fn main() {
-    ch_15::ch_15_05::ch_15_05_01::ch_15_05_01_01::fn_15_05_01_01();
+    ch_15::ch_15_05::ch_15_05_01::ch_15_05_01_02::fn_15_05_01_02();
 }
