eae12664cfd4325fdd540666369b5e317e817ec2
luotao <luotaoyeah@gmail.com>
2018-11-11 08:43:28 +0800

    the-rust-programming-language: 16.2: sending multiple values and seeing the receiver waiting

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use books::the_rust_programming_language_2018e::ch_16;
 mod books;
 
 fn main() {
-    ch_16::ch_16_02::ch_16_02_01::ch_16_02_01_02::fn_16_02_01_02();
+    ch_16::ch_16_02::ch_16_02_01::ch_16_02_01_03::fn_16_02_01_03();
 }
