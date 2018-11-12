1c5d957023cdee0e16b7c3e0621c9e8a628ec4cf
luotao <luotaoyeah@gmail.com>
2018-11-12 18:39:36 +0800

    the-rust-programming-language: 17.1: inheritance as a type system and as code sharing

---
+++ src/books/the_rust_programming_language_2018e/ch_17/ch_17_01/ch_17_01_01/ch_17_01_01_02.rs
@@ -0,0 +1,18 @@
+/*
+  Object Oriented Programming Features of Rust
+      Characteristics of Object-Oriented Languages
+          Inheritance as a Type System and as Code Sharing
+*/
+
+pub fn fn_17_01_01_01() {
+    println!("-------------------------------------------------- 01");
+    {
+        /*
+          rust 没有直接支持 inheritance，
+          但是可以通过 trait 的 default method implementation 来实现代码共享，
+          同时可以 override default implementation；
+        
+          rust 通过 trait object 来支持 polymorphism；
+        */
+    }
+}
