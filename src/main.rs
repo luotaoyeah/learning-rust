53d08c71a3babb3b1437c84266a2867588e9b8fe
luotao <luotaoyeah@gmail.com>
2018-11-13 19:15:39 +0800

    the-rust-programming-language: 17.3: requesting a review of the post changes its state

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use books::the_rust_programming_language_2018e::ch_17;
 mod books;
 
 fn main() {
-    ch_17::ch_17_03::ch_17_03_01::ch_17_03_01_03::fn_17_03_01_03();
+    ch_17::ch_17_03::ch_17_03_01::ch_17_03_01_04::fn_17_03_01_04();
 }
