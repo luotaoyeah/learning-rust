f6f95d9f2c3e3d809b48f8d30cc482edd0ecaf3a
luotao <luotaoyeah@gmail.com>
2018-11-14 20:03:26 +0800

    the-rust-programming-language: 18.1: conditional if let expressions

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use books::the_rust_programming_language_2018e::ch_18;
 mod books;
 
 fn main() {
-    ch_18::ch_18_01::ch_18_01_01::ch_18_01_01_01::fn_18_01_01_01();
+    ch_18::ch_18_01::ch_18_01_01::ch_18_01_01_02::fn_18_01_01_02();
 }
