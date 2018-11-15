4e5219cf34f123396a7d949482cfd10782fbb874
luotao <luotaoyeah@gmail.com>
2018-11-15 19:31:38 +0800

    the-rust-programming-language: 18.1: for loops

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use books::the_rust_programming_language_2018e::ch_18;
 mod books;
 
 fn main() {
-    ch_18::ch_18_01::ch_18_01_01::ch_18_01_01_03::fn_18_01_01_03();
+    ch_18::ch_18_01::ch_18_01_01::ch_18_01_01_04::fn_18_01_01_04();
 }
