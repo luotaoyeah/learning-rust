c67e5d6ddc7a8713c12409642e79a075cc3b9be1
luotao <luotaoyeah@gmail.com>
2018-11-17 09:05:47 +0800

    the-rust-programming-language: 18.3: destructuring structs and tuples

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use books::the_rust_programming_language_2018e::ch_18;
 mod books;
 
 fn main() {
-    ch_18::ch_18_03::ch_18_03_02::ch_18_03_02_04::fn_18_03_02_04();
+    ch_18::ch_18_03::ch_18_03_02::ch_18_03_02_05::fn_18_03_02_05();
 }
