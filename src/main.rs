d17c3025b1fc95cbba673beda7699963746ccc8d
luotao <luotaoyeah@gmail.com>
2018-11-17 09:38:40 +0800

    the-rust-programming-language: 18.3: ignoring remaining parts of a value with ..

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use books::the_rust_programming_language_2018e::ch_18;
 mod books;
 
 fn main() {
-    ch_18::ch_18_03::ch_18_03_03::ch_18_03_03_03::fn_18_03_03_03();
+    ch_18::ch_18_03::ch_18_03_03::ch_18_03_03_04::fn_18_03_03_04();
 }
