b7df873a346bc85c947eaadc35adef7336f39a41
luotao <luotaoyeah@gmail.com>
2018-11-18 15:49:12 +0800

    the-rust-programming-language: 19.1: advanced lifetimes

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use books::the_rust_programming_language_2018e::ch_19;
 mod books;
 
 fn main() {
-    ch_19::ch_19_01::ch_19_01_03::ch_19_01_03_01::fn_19_01_03_01();
+    ch_19::ch_19_02::ch_19_02_01::ch_19_02_01_01::fn_19_02_01_01();
 }
