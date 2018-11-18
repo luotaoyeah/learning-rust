887fae3e09a2bd7bdb15833c141176e14d67a3ef
luotao <luotaoyeah@gmail.com>
2018-11-18 14:40:55 +0800

    the-rust-programming-language: 19.1: using extern functions to call external code

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use books::the_rust_programming_language_2018e::ch_19;
 mod books;
 
 fn main() {
-    ch_19::ch_19_01::ch_19_01_01::ch_19_01_01_03::fn_19_01_01_03();
+    ch_19::ch_19_01::ch_19_01_01::ch_19_01_01_04::fn_19_01_01_04();
 }
