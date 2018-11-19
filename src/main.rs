043759d56c9a2b6898aed5366705cd3dd5157610
luotao <luotaoyeah@gmail.com>
2018-11-19 19:44:09 +0800

    the-rust-programming-language: 19.5: returning closures

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use books::the_rust_programming_language_2018e::ch_19;
 mod books;
 
 fn main() {
-    ch_19::ch_19_05::ch_19_05_01::ch_19_05_01_01::fn_19_04_01_01();
+    ch_19::ch_19_05::ch_19_05_02::ch_19_05_02_01::fn_19_05_02_01();
 }
