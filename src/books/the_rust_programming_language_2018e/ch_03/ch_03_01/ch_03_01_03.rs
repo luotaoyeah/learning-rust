224d89f6a1a579b2bb39638b0e5d4e7043a88277
luotao <luotaoyeah@gmail.com>
2018-11-21 21:39:22 +0800

    the-rust-programming-language: review: shadowing

--- src/books/the_rust_programming_language_2018e/ch_03/ch_03_01/ch_03_01_03.rs
+++ src/books/the_rust_programming_language_2018e/ch_03/ch_03_01/ch_03_01_03.rs
@@ -1,21 +1,28 @@
 /*
-   Common Programming Concepts
-       Shadowing
- */
+  Common Programming Concepts
+      Shadowing
+*/
 
 pub fn fn_03_01_03() {
-    /*
-       使用 let 重复声明同名的变量，后面的变量会隐藏之前声明的变量，称之为 shadowing；
-     */
-    let x = 5;
-    let x = x + 1;
-    let x = x * 2;
-    println!("{}", x);
+    println!("-------------------------------------------------- 01");
+    {
+        /*
+          使用 let 重复声明同名的变量，后面的变量会隐藏之前声明的变量，称之为 shadowing；
+        */
+        let x = 5;
+        let x = x + 1;
+        let x = x * 2;
+        println!("{}", x); // 12
+    }
 
-    /*
-       shadowing 的时候，可以改变变量的类型；
-     */
-    let spaces = "     ";
-    let spaces = spaces.len();
-    println!("{}", spaces);
+    println!("-------------------------------------------------- 02");
+    {
+        /*
+          shadowing 的时候，可以使用不同的类型；
+        */
+        let spaces = "foo";
+        let mut spaces = spaces.len();
+        spaces += 1;
+        println!("{}", spaces); // 4
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use books::the_rust_programming_language_2018e::ch_03;
 mod books;
 
 fn main() {
-    ch_03::ch_03_01::ch_03_01_02::fn_03_01_02();
+    ch_03::ch_03_01::ch_03_01_03::fn_03_01_03();
 }
