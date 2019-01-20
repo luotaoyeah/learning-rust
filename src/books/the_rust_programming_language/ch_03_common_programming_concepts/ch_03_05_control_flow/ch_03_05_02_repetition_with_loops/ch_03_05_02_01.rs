108b0196b034836418b07e28e7ec5357355f7e5a
luotao <luotaoyeah@gmail.com>
2019-01-20 17:02:22 +0800

    the-rust-programming-language: 03-05: loop

--- src/books/the_rust_programming_language/ch_03_common_programming_concepts/ch_03_05_control_flow/ch_03_05_02_repetition_with_loops/ch_03_05_02_01.rs
+++ src/books/the_rust_programming_language/ch_03_common_programming_concepts/ch_03_05_control_flow/ch_03_05_02_repetition_with_loops/ch_03_05_02_01.rs
@@ -1,9 +1,25 @@
 /*
   Control Flow
       Repetition with Loops
+          Repeating Code with loop
 */
 
 pub fn fn_03_05_02_01() {
     println!("-------------------------------------------------- 01");
-    // TODO
+    // 使用 loop 关键字定义一个 loop block 代码块，
+    // 代码块中的代码会不断循环执行，除非使用 break 显式地终止循环
+
+    let mut i: u32 = 0;
+
+    // 1
+    // 2
+    // 3
+    loop {
+        if i >= 3 {
+            break;
+        }
+
+        i += 1;
+        println!("{}", i);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,18 +11,18 @@ fn type_of<T>(_: &T) {
 }
 
 fn main() {
-        beginning_rust
-            ::ch_02_doing_arithmetic
-            ::ch_02_05_breaking_literal_strings
-            ::ch_02_05_03
-            ::fn_02_05_03();
+    /*
+            beginning_rust
+                ::ch_02_doing_arithmetic
+                ::ch_02_05_breaking_literal_strings
+                ::ch_02_05_03
+                ::fn_02_05_03();
+    */
 
-/*
     the_rust_programming_language
             ::ch_03_common_programming_concepts
             ::ch_03_05_control_flow
             ::ch_03_05_02_repetition_with_loops
             ::ch_03_05_02_01
             ::fn_03_05_02_01()
-*/
 }
