7046ccf4209df3184b2c0162e5e6f69fbdf5f711
luotao <luotaoyeah@gmail.com>
2019-01-29 18:48:22 +0800

    beginning-rust: 03-04-01: uninitialized variables

--- src/books/beginning_rust/ch_03_naming_objects.rs
+++ src/books/beginning_rust/ch_03_naming_objects.rs
@@ -1,3 +1,4 @@
 pub mod ch_03_01_associating_names_to_values;
 pub mod ch_03_02_mutable_variables;
 pub mod ch_03_03_not_mutated_mutable_variables;
+pub mod ch_03_04_uninitialized_variables;
---
+++ src/books/beginning_rust/ch_03_naming_objects/ch_03_04_uninitialized_variables/ch_03_04_01.rs
@@ -0,0 +1,22 @@
+/*
+  Naming Objects
+      3.4. Uninitialized Variables
+*/
+
+pub fn fn_03_04_01() {
+    println!("-------------------------------------------------- 01");
+    // 变量可以在声明的同时就进行初始化
+    let i01: u32 = 9;
+    println!("{}", i01);
+
+    // 变量也可以先声明，再初始化
+    let i02: u32;
+    i02 = 9;
+    println!("{}", i02);
+
+    // 如果一个变量没有进行初始化就被使用，则会编译报错
+    let i03: u32;
+    /*
+        println!("{}", i03); // error[E0381]: borrow of possibly uninitialized variable: `i03`
+    */
+}
